100cc41c2a57b51e431055a6854fbeb664023f65
Mohamed Desouky <mohameddesouky@Mohameds-MacBook-Pro.local>
2021-08-14 13:33:12 +0700

    Add VectorIcons and SVG

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import SvgUri from 'react-native-svg-uri';
 
 const App: React.FC = () => {
   return (
     <View>
       <Text>Hello, world!</Text>
+      <SvgUri
+        width={200}
+        height={200}
+        source={{
+          uri: 'https://thenewcode.com/assets/images/thumbnails/homer-simpson.svg',
+        }}
+      />
     </View>
   );
 };
