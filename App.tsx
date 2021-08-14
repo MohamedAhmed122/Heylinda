8e81bd4769d0997aa4622b87a4facbecf392c91d
Mohamed Desouky <mohameddesouky@Mohameds-MacBook-Pro.local>
2021-08-14 12:59:30 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+};
+
+export default App;
+const styles = StyleSheet.create({});
---
+++ src/config/color.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  primary: '#F25835',
+  secondary: '#5A86BF',
+  danger: '#F23207',
+  success: '#048C19',
+  beige: '#D9CA9C',
+  gray: '#828282',
+  lightGray: '#c7c6c1',
+  lightGrayPlus: '#D9dddc',
+  white: '#F2F2F2',
+  black: '#0D0D0D',
+};
